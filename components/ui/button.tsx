eec719e8650c98112eb9e1cc3806ba9f63411646
AshwiniParaye1 <ashwiniparaye9309@gmail.com>
2024-06-30 10:00:00 +0530

    debugging

--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -43,6 +43,7 @@ const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
   ({ className, variant, size, asChild = false, ...props }, ref) => {
     console.log(className);
     console.log(variant);
+    console.log(size);
 
     const Comp = asChild ? Slot : "button";
     return (

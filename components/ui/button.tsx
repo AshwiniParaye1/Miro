257221b414238a25727a4ab61d534e9ebed25b40
AshwiniParaye1 <ashwiniparaye9309@gmail.com>
2024-08-30 15:30:48 +0530

    added console

--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -41,6 +41,8 @@ export interface ButtonProps
 
 const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
   ({ className, variant, size, asChild = false, ...props }, ref) => {
+    console.log(className);
+
     const Comp = asChild ? Slot : "button";
     return (
       <Comp

1a5cb5f2b38805d1aed407a06ad7de66f6df6ab0
AshwiniParaye1 <ashwiniparaye9309@gmail.com>
2024-09-02 23:07:55 +0530

    fix key issue

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,16 @@
 import { Button } from "@/components/ui/button";
+import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/nextjs";
 
 export default function Home() {
   return (
     <>
       <Button>Click here</Button>
+      <SignedOut>
+        <SignInButton />
+      </SignedOut>
+      <SignedIn>
+        <UserButton />
+      </SignedIn>
     </>
   );
 }

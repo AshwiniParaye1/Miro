12e1d4ecebcbac9f1fe0479d42f85a18914bfe3a
AshwiniParaye1 <ashwiniparaye9309@gmail.com>
2024-09-04 16:52:56 +0530

    styled sidebar component

--- app/(dashboard)/_components/sidebar/index.tsx
+++ app/(dashboard)/_components/sidebar/index.tsx
@@ -1,3 +1,7 @@
 export const Sidebar = () => {
-  return <div>sidebar</div>;
+  return (
+    <aside className="fixed z-[1] left-0 bg-blue-950 w-[60px] h-full flex flex-col gap-y-4 p-3 text-white">
+      Side
+    </aside>
+  );
 };

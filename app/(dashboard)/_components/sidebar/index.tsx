e4f997307d86428a40ff8180faa488cd4b1cc550
AshwiniParaye1 <ashwiniparaye9309@gmail.com>
2024-09-04 17:32:45 +0530

    added shadcn buttons

--- app/(dashboard)/_components/sidebar/index.tsx
+++ app/(dashboard)/_components/sidebar/index.tsx
@@ -1,7 +1,9 @@
+import { NewButton } from "./new-button";
+
 export const Sidebar = () => {
   return (
     <aside className="fixed z-[1] left-0 bg-blue-950 w-[60px] h-full flex flex-col gap-y-4 p-3 text-white">
-      Side
+      <NewButton />
     </aside>
   );
 };

e275744658a3bcc11c632b8740b36798fa586fdc
AshwiniParaye1 <ashwiniparaye9309@gmail.com>
2024-09-05 17:50:41 +0530

    created team and favorite board buttons

--- app/(dashboard)/_components/org-sidebar.tsx
+++ app/(dashboard)/_components/org-sidebar.tsx
@@ -1,7 +1,9 @@
 "use client";
 
+import { Button } from "@/components/ui/button";
 import { cn } from "@/lib/utils";
 import { OrganizationSwitcher } from "@clerk/nextjs";
+import { LayoutDashboard, Star } from "lucide-react";
 import { Poppins } from "next/font/google";
 import Image from "next/image";
 import Link from "next/link";
@@ -19,7 +21,56 @@ export const OrgSidebar = () => {
           </span>
         </div>
       </Link>
-      <OrganizationSwitcher hidePersonal />
+      <OrganizationSwitcher
+        hidePersonal
+        appearance={{
+          elements: {
+            rootBox: {
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+              width: "100%",
+            },
+            organizationSwitcherTrigger: {
+              padding: "6px",
+              width: "100%",
+              borderRadius: "8px",
+              border: "1px solid #E5E7EB",
+              justifyContent: "space-between",
+              backgroundColor: "white",
+            },
+          },
+        }}
+      />
+      <div className="space-y-1 w-full">
+        <Button
+          variant={"ghost"}
+          asChild
+          size="lg"
+          className="font-normal justify-start px-2 w-full"
+        >
+          <Link href="/">
+            <LayoutDashboard className="h-4 w-4 mr-2" />
+            Team Boards
+          </Link>
+        </Button>
+        <Button
+          variant={"ghost"}
+          asChild
+          size="lg"
+          className="font-normal justify-start px-2 w-full"
+        >
+          <Link
+            href={{
+              pathname: "/",
+              query: { favorites: "true" },
+            }}
+          >
+            <Star className="h-4 w-4 mr-2" />
+            Favorite Boards
+          </Link>
+        </Button>
+      </div>
     </div>
   );
 };

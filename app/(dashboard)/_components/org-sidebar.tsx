a6762e1f0dc9676a9e715ffc045649bc63e7abdb
AshwiniParaye1 <ashwiniparaye9309@gmail.com>
2024-09-05 17:17:16 +0530

    added organization switcher from clerk

--- app/(dashboard)/_components/org-sidebar.tsx
+++ app/(dashboard)/_components/org-sidebar.tsx
@@ -1,9 +1,25 @@
 "use client";
 
+import { cn } from "@/lib/utils";
+import { OrganizationSwitcher } from "@clerk/nextjs";
+import { Poppins } from "next/font/google";
+import Image from "next/image";
+import Link from "next/link";
+
+const font = Poppins({ subsets: ["latin"], weight: "600" });
+
 export const OrgSidebar = () => {
   return (
-    <div className="hidden lg:flex flex-col spave-y-6 w-[206px] pl-5 pt-5 bg-red-500">
-      OrgSidebar
+    <div className="hidden lg:flex flex-col space-y-6 w-[206px] pl-5 pt-5 ">
+      <Link href="/">
+        <div className="flex items-center gap-x-2">
+          <Image src="/logo.svg" alt="Logo" width={60} height={60} />
+          <span className={cn("font-semibold text-2xl", font.className)}>
+            Board
+          </span>
+        </div>
+      </Link>
+      <OrganizationSwitcher />
     </div>
   );
 };

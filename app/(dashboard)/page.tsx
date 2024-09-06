0e9e71b3b610bfcb6e264dc9e528da525c4d93e0
AshwiniParaye1 <ashwiniparaye9309@gmail.com>
2024-09-06 19:06:29 +0530

    empty board list created

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -3,6 +3,7 @@
 import React from "react";
 import { EmptyOrg } from "./_components/empty-org";
 import { useOrganization } from "@clerk/nextjs";
+import { BoardList } from "./_components/board-list";
 
 interface DashboardPageProps {
   searchParams: {
@@ -16,8 +17,11 @@ const DashboardPage = ({ searchParams }: DashboardPageProps) => {
 
   return (
     <div className="flex-1 h-[calc(100vh-80px)] p-6">
-      {JSON.stringify(searchParams)}
-      {!organization ? <EmptyOrg /> : <p>Board List!</p>}
+      {!organization ? (
+        <EmptyOrg />
+      ) : (
+        <BoardList orgId={organization.id} query={searchParams} />
+      )}
     </div>
   );
 };

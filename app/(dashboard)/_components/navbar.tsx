5bdccc596b284b400b512eda2a3c6fa62483fe45
AshwiniParaye1 <ashwiniparaye9309@gmail.com>
2024-09-06 17:44:57 +0530

    modified search styling

--- app/(dashboard)/_components/navbar.tsx
+++ app/(dashboard)/_components/navbar.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { UserButton } from "@clerk/nextjs";
+import { OrganizationSwitcher, UserButton } from "@clerk/nextjs";
 import { SearchInput } from "./search-input";
 
 export const Navbar = () => {
@@ -9,9 +9,30 @@ export const Navbar = () => {
       <div className="hidden lg:flex lg:flex-1 ">
         <SearchInput />
       </div>
-      <div>
-        <UserButton />
-      </div>
+      <div className="block lg:hidden flex-1"></div>
+      <OrganizationSwitcher
+        hidePersonal
+        appearance={{
+          elements: {
+            rootBox: {
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+              width: "100%",
+              maxWidth: "367px",
+            },
+            organizationSwitcherTrigger: {
+              padding: "6px",
+              width: "100%",
+              borderRadius: "8px",
+              border: "1px solid #E5E7EB",
+              justifyContent: "space-between",
+              backgroundColor: "white",
+            },
+          },
+        }}
+      />
+      <UserButton />
     </div>
   );
 };

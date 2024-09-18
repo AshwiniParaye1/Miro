48919075a547fda5a24954de357f8ad9014d7a07
AshwiniParaye1 <ashwiniparaye9309@gmail.com>
2024-09-18 16:54:45 +0530

    added footer styling

--- app/(dashboard)/_components/board-card/footer.tsx
+++ app/(dashboard)/_components/board-card/footer.tsx
@@ -18,5 +18,12 @@ export const Footer = ({
   onClick,
   disabled,
 }: FooterProps) => {
-  return <div>footer</div>;
+  return (
+    <div className="relative bg-white p-3">
+      <p className="text-[13px] truncate max-w-[calc(100%-20px)]">{title}</p>
+      <p className="opacity-0 group-hover:opacity-100 transition-opacity text-[11px] truncate text-muted-foreground">
+        {authorLabel}, {createdAtLabel}
+      </p>
+    </div>
+  );
 };

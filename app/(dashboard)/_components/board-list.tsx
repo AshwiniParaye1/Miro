9a2aa34cfade0320a6055ba283e1f1ae9d79a883
AshwiniParaye1 <ashwiniparaye9309@gmail.com>
2024-09-18 17:49:51 +0530

    show loading state only when undefined

--- app/(dashboard)/_components/board-list.tsx
+++ app/(dashboard)/_components/board-list.tsx
@@ -19,7 +19,7 @@ interface BoardListProps {
 export const BoardList = ({ orgId, query }: BoardListProps) => {
   const data = useQuery(api.boards.get, { orgId });
 
-  if (true) {
+  if (data === undefined) {
     return (
       <div>
         <h2 className="text-3xl">

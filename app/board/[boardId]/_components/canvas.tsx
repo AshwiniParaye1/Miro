ede81d25091e21b081387476ed509e10f11a3888
AshwiniParaye1 <ashwiniparaye9309@gmail.com>
2024-09-19 19:35:53 +0530

    toolbar compo added

--- app/board/[boardId]/_components/canvas.tsx
+++ app/board/[boardId]/_components/canvas.tsx
@@ -2,12 +2,14 @@
 
 import { Info } from "./info";
 import { Participants } from "./participants";
+import { Toolbar } from "./toolbar";
 
 export const Canvas = () => {
   return (
     <main className="w-full h-full relative bg-neutral-100 touch-none">
       <Info />
       <Participants />
+      <Toolbar />
     </main>
   );
 };

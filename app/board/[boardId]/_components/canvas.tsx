fdd945ff89569e66da54e75dea9c9afd68de346c
AshwiniParaye1 <ashwiniparaye9309@gmail.com>
2024-09-19 19:16:00 +0530

    info compo added

--- app/board/[boardId]/_components/canvas.tsx
+++ app/board/[boardId]/_components/canvas.tsx
@@ -1,7 +1,11 @@
 "use client";
 
+import { Info } from "./info";
+
 export const Canvas = () => {
   return (
-    <main className="w-full h-full relative bg-neutral-100 touch-none"></main>
+    <main className="w-full h-full relative bg-neutral-100 touch-none">
+      <Info />
+    </main>
   );
 };

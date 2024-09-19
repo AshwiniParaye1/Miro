64f969a7aea129450d6bffb3afaf7ff72911ad6b
AshwiniParaye1 <ashwiniparaye9309@gmail.com>
2024-09-19 19:26:51 +0530

    participants compo added

--- app/board/[boardId]/_components/canvas.tsx
+++ app/board/[boardId]/_components/canvas.tsx
@@ -1,11 +1,13 @@
 "use client";
 
 import { Info } from "./info";
+import { Participants } from "./participants";
 
 export const Canvas = () => {
   return (
     <main className="w-full h-full relative bg-neutral-100 touch-none">
       <Info />
+      <Participants />
     </main>
   );
 };
--- app/board/[boardId]/_components/info.tsx
+++ app/board/[boardId]/_components/info.tsx
@@ -3,7 +3,7 @@ import React from "react";
 export const Info = () => {
   return (
     <div className="absolute top-2 left-2 bg-white rounded-md px-1.5 h-12 flex items-center shadow-md">
-      Info
+      Todo - info about the board
     </div>
   );
 };

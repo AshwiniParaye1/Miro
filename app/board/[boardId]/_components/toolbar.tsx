114f9b6a2c6beac0da47e33e76fc33ca8e6976b2
AshwiniParaye1 <ashwiniparaye9309@gmail.com>
2024-09-19 19:48:10 +0530

    fixed layout issue

--- app/board/[boardId]/_components/toolbar.tsx
+++ app/board/[boardId]/_components/toolbar.tsx
@@ -2,8 +2,8 @@ import React from "react";
 
 export const Toolbar = () => {
   return (
-    <div className="absolute top-[50%] -translate-y-[50%] flex flex-col gap-y-4 left-2">
-      <div className="bg-white rounded-md p-1.5 flex gap-y-1 flex-col items-center drop-shadow-md">
+    <div className="absolute top-[-50%] -translate-y-[-50%] left-2 flex flex-col gap-y-4">
+      <div className="bg-white rounded-md p-1.5 flex gap-1 flex-col items-center drop-shadow-md">
         <div>Pendil</div>
         <div>Square</div>
         <div>Circle</div>

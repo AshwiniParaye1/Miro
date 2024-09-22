5d21cf7caf997edff4dbf1eda38a9ab2e884bf91
AshwiniParaye1 <ashwiniparaye9309@gmail.com>
2024-09-22 10:00:00 +0530

    created aoi route

---
+++ app/api/liveblocks-auth/route.ts
@@ -0,0 +1,9 @@
+import { Liveblocks } from "@liveblocks/node";
+import { ConvexHttpClient } from "convex/browser";
+import { api } from "@/convex/_generated/api";
+
+const convex = new ConvexHttpClient(process.env.NEXT_PUBLIC_CONVEX_URL!);
+
+const liveblocks = new Liveblocks({
+  secret: process.env.LIVEBLOCKS_SECRET_KEY!,
+});

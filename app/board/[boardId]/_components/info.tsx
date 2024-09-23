6e5d7f0c78ca1de8ab903729ce5714d0e3b70975
AshwiniParaye1 <ashwiniparaye9309@gmail.com>
2024-09-23 19:28:00 +0530

    added rename modal to button

--- app/board/[boardId]/_components/info.tsx
+++ app/board/[boardId]/_components/info.tsx
@@ -8,6 +8,8 @@ import { Poppins } from "next/font/google";
 import { cn } from "@/lib/utils";
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
+import { Hint } from "@/components/hint";
+import { useRenameModal } from "@/store/use-rename-modal";
 
 interface InfoProps {
   boardId: string;
@@ -15,7 +17,13 @@ interface InfoProps {
 
 const font = Poppins({ subsets: ["latin"], weight: "600" });
 
+const TabSeparator = () => {
+  return <div className="text-neutral-300 px-1.5">|</div>;
+};
+
 export const Info = ({ boardId }: InfoProps) => {
+  const { onOpen } = useRenameModal();
+
   const data = useQuery(api.board.get, {
     id: boardId as Id<"boards">,
   });
@@ -26,18 +34,28 @@ export const Info = ({ boardId }: InfoProps) => {
 
   return (
     <div className="absolute top-2 left-2 bg-white rounded-md px-1.5 h-12 flex items-center shadow-md">
-      <Button asChild variant={"board"} className="px-2">
-        <Link href="/">
-          <Image src="/logo.svg" alt="Logo" width={40} height={40} />
-          <span
-            className={cn(
-              "font-semibold text-xl ml-2 text-black",
-              font.className
-            )}
-          >
-            Board
-          </span>
-        </Link>
+      <Hint label="Go to boards" side="bottom" sideOffset={10}>
+        <Button asChild variant={"board"} className="px-2">
+          <Link href="/">
+            <Image src="/logo.svg" alt="Logo" width={40} height={40} />
+            <span
+              className={cn(
+                "font-semibold text-xl ml-2 text-black",
+                font.className
+              )}
+            >
+              Board
+            </span>
+          </Link>
+        </Button>
+      </Hint>
+      <TabSeparator />
+      <Button
+        onClick={() => onOpen(data?._id, data?.title)}
+        variant={"board"}
+        className="text-base font-normal px-2"
+      >
+        {data.title}
       </Button>
     </div>
   );

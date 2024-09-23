5d606dad724327ca00a84dcaf6fe1763eafc4846
AshwiniParaye1 <ashwiniparaye9309@gmail.com>
2024-09-23 18:53:13 +0530

    fixed toolbar not showing issue

--- app/board/[boardId]/_components/toolbar.tsx
+++ app/board/[boardId]/_components/toolbar.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export const Toolbar = () => {
   return (
-    <div className="absolute top-[-50%] -translate-y-[-50%] left-2 flex flex-col gap-y-4">
+    <div className="absolute top-[50%] -translate-y-[50%] left-2 flex flex-col gap-y-4">
       <div className="bg-white rounded-md p-1.5 flex gap-1 flex-col items-center drop-shadow-md">
         <div>Pendil</div>
         <div>Square</div>

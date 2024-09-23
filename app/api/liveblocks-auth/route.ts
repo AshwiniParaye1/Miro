67404d9cbab7892299048957404b55a3e1facd36
AshwiniParaye1 <ashwiniparaye9309@gmail.com>
2024-09-23 12:09:51 +0530

    room and auth sessions created

--- app/api/liveblocks-auth/route.ts
+++ app/api/liveblocks-auth/route.ts
@@ -1,9 +1,55 @@
 import { Liveblocks } from "@liveblocks/node";
 import { ConvexHttpClient } from "convex/browser";
 import { api } from "@/convex/_generated/api";
+import { auth, currentUser } from "@clerk/nextjs/server";
 
 const convex = new ConvexHttpClient(process.env.NEXT_PUBLIC_CONVEX_URL!);
 
 const liveblocks = new Liveblocks({
   secret: process.env.LIVEBLOCKS_SECRET_KEY!,
 });
+
+export async function POST(request: Request) {
+  const authorization = await auth();
+  const user = await currentUser();
+
+  console.log("auth info === ", { authorization, user });
+
+  if (!authorization || !user) {
+    return new Response("Unauthorized", { status: 403 });
+  }
+
+  const { room } = await request.json();
+  const board = await convex.query(api.board.get, {
+    id: room,
+  });
+
+  console.log("board and room info === ", {
+    room,
+    board,
+    boardOrgId: board?.orgId,
+    userOrgId: authorization.orgId,
+  });
+
+  if (board?.orgId !== authorization.orgId) {
+    return new Response("Unauthorized");
+  }
+
+  const userInfo = {
+    name: user.firstName || "Anonymous",
+    picture: user.imageUrl,
+  };
+
+  console.log("user info === ", { userInfo });
+
+  const session = liveblocks.prepareSession(user.id, { userInfo });
+
+  if (room) {
+    session.allow(room, session.FULL_ACCESS);
+  }
+
+  const { status, body } = await session.authorize();
+  console.log("allowed === ", { status, body });
+
+  return new Response(body, { status });
+}

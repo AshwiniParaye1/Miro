34a985b330df9db14ea2b540442395298f569ff7
AshwiniParaye1 <ashwiniparaye9309@gmail.com>
2024-09-26 19:23:18 +0530

    changed purple code

--- app/board/[boardId]/_components/color-picker.tsx
+++ app/board/[boardId]/_components/color-picker.tsx
@@ -14,7 +14,7 @@ export const ColorPicker = ({ onChange }: ColorPickerProps) => {
       <ColorButton color={{ r: 255, g: 249, b: 177 }} onClick={onChange} />
       <ColorButton color={{ r: 68, g: 202, b: 99 }} onClick={onChange} />
       <ColorButton color={{ r: 39, g: 142, b: 237 }} onClick={onChange} />
-      <ColorButton color={{ r: 155, g: 105, b: 245 }} onClick={onChange} />
+      <ColorButton color={{ r: 160, g: 32, b: 245 }} onClick={onChange} />
       <ColorButton color={{ r: 252, g: 142, b: 42 }} onClick={onChange} />
       <ColorButton color={{ r: 0, g: 0, b: 0 }} onClick={onChange} />
       <ColorButton color={{ r: 255, g: 255, b: 255 }} onClick={onChange} />

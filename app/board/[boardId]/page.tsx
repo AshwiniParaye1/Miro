6450fb8a25c88b8d85b8da35e63d83471c355628
AshwiniParaye1 <ashwiniparaye9309@gmail.com>
2024-09-27 20:35:19 +0530

    fixed loader position

--- app/board/[boardId]/page.tsx
+++ app/board/[boardId]/page.tsx
@@ -10,14 +10,7 @@ interface BoardIdPageProps {
 
 const BoardIdPage = ({ params }: BoardIdPageProps) => {
   return (
-    <Room
-      roomId={params.boardId}
-      fallback={
-        <div>
-          <Loading />
-        </div>
-      }
-    >
+    <Room roomId={params.boardId} fallback={<Loading />}>
       <Canvas boardId={params.boardId} />
     </Room>
   );
